88d39e248ebb6f33a80be7439dd7c12ca56a37f9
syakoo <nichijou4009@gmail.com>
2020-11-04 19:08:54 +0900

    add: something

--- backend/sentiment-analysis/__init__.py
+++ backend/sentiment-analysis/__init__.py
@@ -1,22 +1,27 @@
 import logging
+import json
 
 import azure.functions as func
+
+from . import analysis
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    txt = req.params.get('txt')
+    if not txt:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            txt = req_body.get('txt')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    analyzer = analysis.get_analyzer()
+    if txt:
+        pos_or_neg = analyzer(txt)
+        return func.HttpResponse(json.dumps({'pos_or_neg': pos_or_neg}))
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
