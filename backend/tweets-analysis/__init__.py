a8de8d393640c17954958c911f21806a32412724
syakoo <nichijou4009@gmail.com>
2020-11-05 22:10:15 +0900

    update: an add methods to tweets-analysis

--- backend/tweets-analysis/__init__.py
+++ backend/tweets-analysis/__init__.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import json
 
 import azure.functions as func
 
@@ -15,36 +16,46 @@ def main(req: func.HttpRequest, db: func.Out[func.Document], reports: func.Docum
     now_dt = datetime.datetime.now()
     logging.info(f'Python HTTP trigger function processed a request. user_id = {user_id}')
 
-    # ==========
-    # collect tweets
-    logging.info(f'Get tweets and filter them.')
-    logging.info(f'report = {report}')
-    if report:
-        updated_at = datetime.datetime.strptime(report["updated_at"], DATE_FORMAT)
-        tweets = collect_tweets.get_all_tweets(user_id, updated_at)
-    else:
-        tweets = collect_tweets.get_all_tweets(user_id)
-    filtered_tweets = collect_tweets.filter_tweets(tweets)
+    resp_data = []
+    if req.method == 'GET':
+        if not report:
+            return func.HttpResponse(f"report not found. user_id = {user_id}", status_code=404)
 
-    # ==========
-    # analyze tweets
-    logging.info("Analyze tweets.")
-    analyzed_tweets = analyze_tweets.analyze_tweets(filtered_tweets)
+        for tweet_data in report["tweets"]:
+             resp_data.append({**tweet_data})
 
-    # ==========
-    # Add or Update DB
-    if report:
-        new_report = {
-            **report,
-            "updated_at": now_dt.strftime(DATE_FORMAT),
-            "tweets": [*analyzed_tweets, *report['tweets']]
-        }
-    else: 
-        new_report = {
-            "user_id": user_id,
-            "updated_at": now_dt.strftime(DATE_FORMAT),
-            "tweets": analyzed_tweets
-        }
-    db.set(func.Document.from_dict(new_report))
+    elif req.method == 'POST':
+        # ==========
+        # collect tweets
+        logging.info(f'Get tweets and filter them.')
+        logging.info(f'report = {report}')
+        if report:
+            updated_at = datetime.datetime.strptime(report["updated_at"], DATE_FORMAT)
+            tweets = collect_tweets.get_all_tweets(user_id, updated_at)
+        else:
+            tweets = collect_tweets.get_all_tweets(user_id)
+        filtered_tweets = collect_tweets.filter_tweets(tweets)
 
-    return func.HttpResponse(f"SUCCESS")
+        # ==========
+        # analyze tweets
+        logging.info(f"Analyze tweets. len(tweets) = {len(filtered_tweets)}")
+        analyzed_tweets = analyze_tweets.analyze_tweets(filtered_tweets)
+
+        # ==========
+        # Add or Update DB
+        if report:
+            new_report = {
+                **report,
+                "updated_at": now_dt.strftime(DATE_FORMAT),
+                "tweets": [*analyzed_tweets, *report['tweets']]
+            }
+        else: 
+            new_report = {
+                "user_id": user_id,
+                "updated_at": now_dt.strftime(DATE_FORMAT),
+                "tweets": analyzed_tweets
+            }
+        db.set(func.Document.from_dict(new_report))
+        resp_data = new_report["tweets"]
+    
+    return func.HttpResponse(json.dumps({'reports': resp_data}), headers={'Content-Type': 'application/json'})
