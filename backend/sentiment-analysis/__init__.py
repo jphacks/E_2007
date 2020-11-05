a333aa08e34b9923d05fa60d992274dadb8c3a3c
syakoo <nichijou4009@gmail.com>
2020-11-05 14:03:39 +0900

    refactor: sentiment-analysis.__init__.py

--- backend/sentiment-analysis/__init__.py
+++ backend/sentiment-analysis/__init__.py
@@ -7,8 +7,8 @@ from . import analysis
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
+    # ======
+    # get txt
     txt = req.params.get('txt')
     if not txt:
         try:
@@ -17,13 +17,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             txt = req_body.get('txt')
-
-    analyzer = analysis.get_analyzer()
-    if txt:
-        pos_or_neg = analyzer(txt)
-        return func.HttpResponse(json.dumps({'pos_or_neg': pos_or_neg}))
-    else:
+    logging.info(f'sentiment-analysis called: {txt}')
+    if not txt:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a txt in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+    # ======
+    # analyze
+    analyzer = analysis.get_analyzer()
+    pos_or_neg = analyzer(txt)
+    logging.info({'txt': txt, 'pos_or_neg': pos_or_neg})
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    }
+    body_dict = {
+        'pos_or_neg': pos_or_neg
+    }
+    
+    return func.HttpResponse(json.dumps(body_dict), status_code=200, headers=headers)
